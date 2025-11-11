3cb330f3ed14bf892656fc6467816e7bca017fe3
Aditya Dahake <dahakeaditya263@gmail.com>
2025-11-11 18:05:47 +0530

    Update Signup Page - Aditya Dahake

--- frontends/src/components/PageLoader.jsx
+++ frontends/src/components/PageLoader.jsx
@@ -1,10 +1,10 @@
 import { LoaderIcon } from "lucide-react";
-import useThemeStore from "../hook/useThemeStore";
+// import useThemeStore from "../hook/useThemeStore";
 
 const PageLoader = () => {
-  const { theme } = useThemeStore();
+//   const { theme } = useThemeStore();
   return (
-    <div className="min-h-screen flex items-center justify-center" data-theme={theme}>
+    <div className="min-h-screen flex items-center justify-center" /*data-theme={theme}*/>
       <LoaderIcon className="animate-spin size-10 text-primary" />
     </div>
   );
--- frontends/src/hook/useThemeStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand'
-
-const useThemeStore = create((set) => ({
-  theme: 'dark',
-  setTheme: (theme) => set({ theme }),
-}))
-
-export default useThemeStore
